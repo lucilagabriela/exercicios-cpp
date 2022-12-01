e67de67d2faa09dff8cb7f4ca9c2e677a42ec5f6
Lucila Costa <lucilagdac@gmail.com>
2022-12-01 14:46:00 -0300

    up

---
+++ ex002.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    float numero;
+
+    cout << "Digite um número: " << endl;
+    cin >> numero;
+    cout << "O número foi: " << numero << endl;
+    return 0;
+}
---
+++ ex003.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    cout << "Digite o número a: " << endl;
+    cin >> a;
+    cout << "Digite o número b: " << endl;
+    cin >> b;
+    cout << "A soma é: " << a+b << endl;
+
+    return 0;
+}
