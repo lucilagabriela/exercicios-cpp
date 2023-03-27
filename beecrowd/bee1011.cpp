ebe8f6372a9ac55a875154e39cbb7b854cc89989
Lucila Costa <lucilagdac@gmail.com>
2023-03-27 14:49:29 -0300

    Update bee1011.cpp

--- beecrowd/bee1011.cpp
+++ beecrowd/bee1011.cpp
@@ -7,7 +7,7 @@ int main() {
     double raio, volume;
     cin >> raio;
     // volume = (4/3.0)*3,141*(pow(raio, 3));
-    volume = (4/3)*3,141*pow(raio, 3);
+    volume = (4/3)*3,14159*pow(raio, 3);
     // cout << "VOLUME = " << volume << endl;
     printf("VOLUME = %.3f\n", volume);
 
